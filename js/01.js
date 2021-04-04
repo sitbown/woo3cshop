5d8c7104a73ba797b8933fb4c513d8491c154607
sitbown <32032412+sitbown@users.noreply.github.com>
2021-04-04 16:03:11 +0800

    Add files via upload

---
+++ js/01.js
@@ -0,0 +1,12 @@
+
+		// $(document).ready(function(){
+		// 	$("#slider").after("<div id='nav'>").cycle({
+		// 		fx:"scrollHorz",//效果
+		// 		timeout:3000,//停留時間
+		// 		speed:300,//切換速度
+		// 		pause:1,//滑鼠滑過暫停輪播
+		// 		pager:"#nav"
+
+		// 	});
+		// });
+	
